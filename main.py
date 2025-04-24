3fe6e372ca375306f7cf2b4189a498f9beddea69
Eystill <dan.dalsgaard.hansen@gmail.com>
2025-04-24 14:43:26 +0200

    Creating the game window main file and importing modules from pygame

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+	pygame.init()
+	# Splash screen showing that pygame module runs and the screen resolution set in constants
 	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+
+	# Initialize game area with the resolution specified in constants
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	# creating an infinite loop
+	while True:
+		# Draw a black square inside the game area
+		pygame.Surface.fill(screen, (0,0,0))
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
