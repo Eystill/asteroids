e5d82146dd3e9603633eeed62f32c74ed3aae0ae
Eystill <dan.dalsgaard.hansen@gmail.com>
2025-05-05 16:09:09 +0200

    Added asteroid field and asteroid files and methods

--- player.py
+++ player.py
@@ -29,10 +29,20 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
-    # When the key a is pressed turn left (-dt) when the key d is pressed turn right (dt)
+    # When the key a is pressed turn left calling the rotate method with (-dt) when the key d is pressed turn right calling the rotate method with (dt)
+    # When the keys w and s are pressed respectively, the player moves backwards and forwards.
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    # Method to move the player forward or backwards
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_MOVE_SPEED * dt
